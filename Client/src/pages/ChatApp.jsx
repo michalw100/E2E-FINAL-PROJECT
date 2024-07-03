c078b4e06146db5f7ad6d6e38081721a8bb73c7d
michalw100 <m0583295188@gmail.com>
2024-07-03 19:05:51 +0300

    מיכל תותחית הצליחה לסדר צאט נורמליי

--- Client/src/pages/ChatApp.jsx
+++ Client/src/pages/ChatApp.jsx
@@ -7,15 +7,43 @@ import {
   Thread,
   Window,
 } from "stream-chat-react";
-import { StreamChat } from "stream-chat";
 import "stream-chat-react/dist/css/index.css";
 import ChannelListContainer from "../components/ChanelList";
 import ChannelMessages from "../components/Messages";
 import "../css/chat.css";
+import MyImage from '../pictures/loading-bar.png';
+
+// const ChatApp = () => {
+//  const { chatClient, clientReady } = useContext(AuthContext);
+//   if (!clientReady) return <img className="loading" src={MyImage}/>;
+//   return (
+//     <Chat client={chatClient} theme="messaging light">
+//       <div className="chat-container">
+//         <div className="channel-list-container">
+//           <ChannelListContainer />
+//         </div>
+//         <div className="channel-container">
+//           <Channel>
+//             <div className="window-container">
+//               <Window>
+//                 <ChannelHeader />
+//                 <ChannelMessages />
+//               </Window>
+//             </div>
+//             <Thread />
+//           </Channel>
+//         </div>
+//       </div>
+//     </Chat>
+//   );
+// };
+
 
 const ChatApp = () => {
- const { chatClient, clientReady } = useContext(AuthContext);
-  if (!clientReady) return <div>Loading...</div>;
+  const { chatClient, clientReady } = useContext(AuthContext);
+
+  if (!clientReady) return <img className="loading" src={MyImage} alt="Loading..." />;
+
   return (
     <Chat client={chatClient} theme="messaging light">
       <div className="chat-container">
