8191ec0e915324300c5eb253a019a01224a0202c
ShiraBraverman <shira7656@gmail.com>
2024-07-04 16:17:57 +0300

    טיפול בלוג אאוט

--- Client/src/AuthContext.jsx
+++ Client/src/AuthContext.jsx
@@ -146,13 +146,13 @@ export const AuthProvider = ({ children }) => {
       if (response.status !== 200) {
         throw new Error(data.message || "An error occurred. Please try again.");
       } else {
-        setUser(null);
-        disconnectClient();
-        navigate("/aboutUs");
       }
     } catch (error) {
       throw new Error(error.message);
     }
+    setUser(null);
+    disconnectClient();
+    navigate("/aboutUs");
   };
 
   return (
