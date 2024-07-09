db589b12c6fca06962eea1ac92c05aebba504ce4
ShiraBraverman <shira7656@gmail.com>
2024-07-10 00:07:16 +0300

    חילוק לשתי קומפוננטות

--- Client/src/pages/UpdatesPage.jsx
+++ Client/src/pages/UpdatesPage.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useState, useContext, useRef } from "react";
 import "../css/update.css";
 import { AuthContext } from "../AuthContext";
-import { useNavigate } from "react-router-dom";
 import {
   FaFileAlt,
   FaComments,
   FaTasks,
   FaClipboardList,
-  FaUser,
-  FaCheckCircle,
-  FaTimesCircle,
-  FaClock,
 } from "react-icons/fa";
 import {
   Chart as ChartJS,
@@ -24,15 +19,7 @@ import {
   PointElement,
 } from "chart.js";
 import { useTranslation } from "react-i18next";
-import MessagesOverview from "../components/MessagesOverview";
-import MessagesPerDay from "../components/MessagesPerDay";
-import MessageCount from "../components/MessageCount";
-import FileCounts from "../components/FileCounts";
-import FilesUploaded from "../components/FilesUploaded";
-import FilesStatus from "../components/FilesStatus";
-import chanels from "../helpers/chanels.js";
-import { MDBBadge } from "mdb-react-ui-kit";
-
+import UpdatesContainer from "../components/UpdatesContainer.jsx";
 ChartJS.register(
   Tooltip,
   Legend,
@@ -52,7 +39,6 @@ function UpdatesPage() {
   const [view, setView] = useState("");
   const highlightRef = useRef(null);
   const buttonsRef = useRef([]);
-  const navigate = useNavigate();
   const { t } = useTranslation();
 
   buttonsRef.current = [];
@@ -68,7 +54,7 @@ function UpdatesPage() {
       highlightRef.current.style.transform = `translateX(${offset}px)`;
       highlightRef.current.style.width = `${buttonRect.width}px`;
     }
-  }, [,view]);
+  }, [, view]);
 
   useEffect(() => {
     setView("fileUpdates");
@@ -146,136 +132,6 @@ function UpdatesPage() {
     }
   };
 
-  const saveMyClient = async (id) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/myClient/storeClientID`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ clientID: id }),
-          credentials: "include",
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-    } catch (error) {
-      toasting(
-        "error",
-        "Error saving user details:" + error.message ? error.message : error
-      );
-    }
-  };
-
-  const handleFileClick = async (event, file) => {
-    event.preventDefault();
-    await saveMyClient(file.clientID);
-    localStorage.setItem("selectedTypeFile", file.type);
-    navigate(`../myFiles/${file.clientID}`, {
-      state: {
-        name: file.name,
-      },
-    });
-  };
-
-  const handleChatClick = async (event, chat) => {
-    event.preventDefault();
-    await chanels.saveCurrentChat(chat.chatId.split("-")[1]);
-    navigate(`../chats/${chat.chatId.split("-")[1]}`);
-  };
-
-  const renderContent = () => {
-    switch (view) {
-      case "fileUpdates":
-        return (
-          <div className="updates">
-            <FileCounts />
-            <FilesUploaded />
-            <FilesStatus />
-          </div>
-        );
-      case "chatUpdates":
-        return (
-          <div className="updates">
-            <MessagesOverview />
-            <MessagesPerDay />
-            <MessageCount />
-          </div>
-        );
-      case "fileTasks":
-        return (
-          <div className="pending-files-container">
-            <h3>{t("Pending Files")}</h3>
-            <div className="pending-files-grid">
-              {pendingFiles &&
-                pendingFiles.map((file) => (
-                  <div key={file.id} className="pending-file-item">
-                    <div className="file-name">
-                      <FaFileAlt className="item-icon" />
-                      {file.name}
-                    </div>
-                    <div className="client-name">
-                      <FaUser className="item-icon" />
-                      {file.clientName}
-                    </div>
-                    <div className={`file-status ${file.status.toLowerCase()}`}>
-                      {t(file.status)}
-                    </div>
-                    <a
-                      href={`http://localhost:5173/myFiles/${file.clientID}`}
-                      onClick={(e) => handleFileClick(e, file)}
-                      className="file-link"
-                    >
-                      {t("View File")}
-                    </a>
-                  </div>
-                ))}
-            </div>
-          </div>
-        );
-      case "chatTasks":
-        return (
-          <div className="pending-chats-container">
-            <h3>{t("Chats")}</h3>
-            <div className="pending-chats-grid">
-              {pendingChats.map((chat) => (
-                <div key={chat.chatId} className="pending-chat-item">
-                  <div className="file-name">
-                    <FaComments className="item-icon" />
-                    {t("Chat")}: {chat.chatName}
-                  </div>
-                  <div className="client-name">
-                    <FaUser className="item-icon" />
-                    {chat.clientName}
-                  </div>
-                  <MDBBadge
-                    pill
-                    color="danger"
-                    className="position-absolute top-0 start-100 translate-middle"
-                  >
-                    {chat.unreadMessagesCount}
-                  </MDBBadge>
-                  <a
-                    href={`/chats/${chat.chatId}`}
-                    onClick={(e) => handleChatClick(e, chat)}
-                    className="chat-link"
-                  >
-                    {t("View Chat")}
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
     <div className="all-update">
       <div className="toggle-buttons">
@@ -322,7 +178,11 @@ function UpdatesPage() {
           {t("Chat Tasks")}
         </button>
       </div>
-      {renderContent()}
+      <UpdatesContainer
+        pendingFiles={pendingFiles}
+        pendingChats={pendingChats}
+        view={view}
+      />{" "}
     </div>
   );
 }
