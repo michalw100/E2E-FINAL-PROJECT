cb008d62ca117bdbc13f62bc4ce26bbe7416b801
ShiraBraverman <shira7656@gmail.com>
2024-07-09 23:51:04 +0300

    העמוד פתיחה עוד איכשהו בסדר

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,22 +3,16 @@ import { MdOutlineMail } from "react-icons/md";
 
 function Footer() {
   return (
-    <footer>
-     
-     <p className="buttomtext">02-6237600 <img
-              className="vectorIcon10"
-              alt=""
-              src="../../src/pictures/phone.svg"
-            /></p>
-     <p className="buttomtext">yael.b@c-b-cpa.co.il <MdOutlineMail />
-     </p>
-     <p className="buttomtext">הכתובת: יעבץ 2, ירושלים <img
-              className="vectorIcon10"
-              alt=""
-              src="../../src/pictures/globus.svg"
-            /></p>
-     {/* <p className="buttomtext">Powered by Michal and Shira.</p> */}
-
+    <footer className="footer">
+      <p className="buttomtext">
+        02-6237600 <img className="vectorIcon10" alt="" src="../../src/pictures/phone.svg" />
+      </p>
+      <p className="buttomtext">
+        yael.b@c-b-cpa.co.il <MdOutlineMail />
+      </p>
+      <p className="buttomtext">
+        הכתובת: יעבץ 2, ירושלים <img className="vectorIcon10" alt="" src="../../src/pictures/globus.svg" />
+      </p>
     </footer>
   );
 }
--- Client/src/pages/AboutUs.jsx
+++ Client/src/pages/AboutUs.jsx
@@ -152,7 +152,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 
-import "../css/aboutUs.css"
+import "../css/about.css";
 import {
   MDBBtn,
   MDBContainer,
@@ -216,137 +216,117 @@ function AboutUs() {
 
   return (
     <div className="about_us">
-      {/* <div className="header_section"> */}
-
-      <img
-        className="img_about_us"
-        src="../src/pictures/FullLogo-removebg-preview.png"
-        alt="Company Logo"
-      />
-
-
-
-      <div className="btnaboutus">
-        <Link to="/signin">
-          <MDBBtn className="mb-4 w-100">שפה  <img
-            className="vectorIcon10"
-            alt=""
-            src="../../src/pictures/globus.svg"
-          /></MDBBtn>
-        </Link>
-        <Link to="/signin">
-          <MDBBtn className="mb-4 w-100">כניסה<img className="frameIcon1" alt="" src="../../src/pictures/Frame.svg" />
-          </MDBBtn>
-        </Link>
-      </div>
-      {/* </div> */}
-
-      <div className="form_section">
-        <div className="contact_info">
-          <img
-            className="img_about_us2"
-            src="../src/pictures/Vector 2.png"
-            alt="Office Image"
-          />
-          <p className="hh">משרד רואי חשבון, יועצי מס והנהלת חשבונות</p>
-          <p className="kk">
-            .משרד רואי החשבון שקרון בק ושות' מעניק שירות לעסקים, עמותות ומלכ"רים, חברות ותאגידים. למשרד מגוון מחלקות מקצועיות הנותנות מענה לכלל צרכי הלקוח בתחום ראיית החשבון
-          </p>
-          <div className="text_section">
-
-            <p className="piska">
-              כגון: מחלקת ביקורת הכוללת בקורת דוחות כספיים, בקורת פנים הכוללת איתור
-              נקודות חולשה בעסק והצעות לפתרונן, בניית מערך בקרה פנימית הולם וייעוץ
-              ארגוני לייעול הבקרה הפנימית של העסק. מחלקת מיסים הכוללת מיסוי שוטף,
-              מיסוי נדל"ן, מיסוי שוק ההון, מיסוי חברות, ייצוג בפני רשויות המס השונות
-              (מס הכנסה, מע"מ ועוד) תכנוני מס וליווי וייעוץ במיסוי הפרט. מחלקת הנהלת
-              חשבונות ומחלקת שכר וכן ייעוץ כלכלי ועסקי הכולל לווי והכוונת עסקים
-              צעירים ומיזמים חדשים, ייעוץ מול הבנקים, בחינת תזרימי מזומנים ותכנון
-              אסטרטגי-עסקי, ליווי בנושאי הקמת תשתיות למחלקת הנהלת החשבונות והשכר.
-              מחלקת ביקורות שכר ומתן מענה מול זרוע העבודה במשרד הכלכלה. ביקורת שכר
-              .בהתאם לתקנות בודק השכר עם התמחות בענפי השמירה, נקיון והסעדה
-            </p>
-          </div>
-        </div>
-
-
-        {/* <MDBCardBody className="px-5"> */}
-        {/* <form onSubmit={sendEmail}>
-
-
-        
-            <MDBInput
-              wrapperClass="mb-4"
-              className="input"
-              label="name"
-              id="form1"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              className="input"
-              label="phone"
-              id="form1"
-              type="text"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              className="input"
-              label="email"
-              id="form2"
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              label="How can we help you?"
-              
-className="input"
-              id="form2"
-              type="text"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-            />
-            <MDBBtn className="mb-4 w-100" type="submit">
-              send
-            </MDBBtn>
-            {error && (
-              <p className="error" style={{ color: "red" }}>
-                {error}
+      <div className="content-wrapper">
+        <MDBContainer fluid className="main-content">
+          <MDBRow>
+            <MDBCol md="6" className="text-content">
+              <div className="header">
+                <img
+                  className="logo"
+                  src="../src/pictures/FullLogo-removebg-preview.png"
+                  alt="Company Logo"
+                />
+                <div className="nav-buttons">
+                  <MDBBtn className="nav-btn square-btn">שפה</MDBBtn>
+                  <MDBBtn className="nav-btn square-btn" on>כניסה</MDBBtn>
+                </div>
+              </div>
+              <h1>משרד רואי חשבון, יועצי מס והנהלת חשבונות</h1>
+              <p>
+                משרד רואי החשבון שקרון בק ושות' מעניק שירות לעסקים, עמותות
+                ומלכ"רים, חברות ותאגידים. למשרד מגוון מחלקות מקצועיות הנותנות
+                מענה לכלל צרכי הלקוח בתחום ראיית החשבון
               </p>
-            )}
-          </form> */}
-
-        {/* </MDBCardBody> */}
-
+              <p className="piska">
+                כגון: מחלקת ביקורת הכוללת בקורת דוחות כספיים, בקורת פנים הכוללת
+                איתור נקודות חולשה בעסק והצעות לפתרונן, בניית מערך בקרה פנימית
+                הולם וייעוץ ארגוני לייעול הבקרה הפנימית של העסק. מחלקת מיסים
+                הכוללת מיסוי שוטף, מיסוי נדל"ן, מיסוי שוק ההון, מיסוי חברות,
+                ייצוג בפני רשויות המס השונות (מס הכנסה, מע"מ ועוד) תכנוני מס
+                וליווי וייעוץ במיסוי הפרט. מחלקת הנהלת חשבונות ומחלקת שכר וכן
+                ייעוץ כלכלי ועסקי הכולל לווי והכוונת עסקים צעירים ומיזמים חדשים,
+                ייעוץ מול הבנקים, בחינת תזרימי מזומנים ותכנון אסטרטגי-עסקי,
+                ליווי בנושאי הקמת תשתיות למחלקת הנהלת החשבונות והשכר. מחלקת
+                ביקורות שכר ומתן מענה מול זרוע העבודה במשרד הכלכלה. ביקורת שכר
+                בהתאם לתקנות בודק השכר עם התמחות בענפי השמירה, נקיון והסעדה
+              </p>
+            </MDBCol>
+            <MDBCol md="6" className="image-content">
+              <img
+                src="../src/pictures/Vector 2.png"
+                alt="Office Image"
+                className="office-image"
+              />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="mt-5">
+            <MDBCol
+              md="6"
+              className="d-flex justify-content-center align-items-center"
+            >
+              <div className="services-list">
+                <div className="services-column">
+                  <p className="lilist">הנהלת חשבונות</p>
+                  <p className="lilist">ביקורת שכר</p>
+                  <p className="lilist">ביקורת פנים</p>
+                </div>
+                <div className="services-column">
+                  <p className="lilist">עמותות ומלכ״רים</p>
+                  <p className="lilist">ייעוץ מיסוי</p>
+                  <p className="lilist">חשבות שכר</p>
+                </div>
+                <div className="services-column">
+                  <p className="lilist">ביקורת דוחות כספיים</p>
+                  <p className="lilist">ייעוץ עסקי</p>
+                  <p className="lilist">תכנוני מיסים והיערכות למס</p>
+                </div>
+              </div>
+            </MDBCol>
+            <MDBCol md="6">
+              <div className="contact-form">
+                <h2>ליצירת קשר</h2>
+                <form onSubmit={sendEmail}>
+                  <MDBRow>
+                    <MDBCol md="6">
+                      <MDBInput
+                        label="שם"
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </MDBCol>
+                    <MDBCol md="6">
+                      <MDBInput
+                        label="טלפון"
+                        type="tel"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </MDBCol>
+                  </MDBRow>
+                  <MDBInput
+                    label="אימייל"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <MDBInput
+                    label="איך נוכל לעזור?"
+                    type="textarea"
+                    rows="3"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  />
+                  <MDBBtn type="submit">שלח</MDBBtn>
+                  {error && <p className="error">{error}</p>}
+                </form>
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>{" "}
       </div>
-      <div className="services-list">
-        <div className="services-column">
-          <p className="lilist">   הנהלת חשבונות</p>
-          <p className="lilist">   ביקורת שכר</p>
-          <p className="lilist">   ביקורת פנים</p>
-        </div>
-        <div className="services-column">
-          <p className="lilist">  עמותות ומלכ״רים</p>
-          <p className="lilist">  ייעוץ מיסוי</p>
-          <p className="lilist">  חשבות שכר</p>
-        </div>
-        <div className="services-column">
-          <p className="lilist"> ביקורת דוחות כספיים</p>
-          <p className="lilist"> ייעוץ עסקי</p>
-          <p className="lilist"> תכנוני מיסים והיערכות למס</p>
-        </div>
-      </div>
-      <Footer></Footer>
-
-      
+      <Footer />
     </div>
-
   );
 }
 
